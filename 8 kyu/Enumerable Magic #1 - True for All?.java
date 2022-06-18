a7eb42171d5ddc192cdcaca772cb13ccef99f944
Carlos M <53409854+Cema2019@users.noreply.github.com>
2022-06-17 18:48:54 -0600

    Create Enumerable Magic #1 - True for All?.java

---
+++ 8 kyu/Enumerable Magic #1 - True for All?.java
@@ -0,0 +1,19 @@
+/*Create a method all which takes two params:
+
+a sequence
+a function (function pointer in C)
+and returns true if the function in the params returns true for every element in the sequence. Otherwise, it should return false. 
+If the sequence is empty, it should return true, since technically nothing failed the test.
+
+Example
+all((1, 2, 3, 4, 5), greater_than_9) -> false
+all((1, 2, 3, 4, 5), less_than_9)    -> True*/
+
+import java.util.function.IntPredicate;
+import java.util.Arrays;
+
+public class Solution{
+    public static boolean all(int[] list, IntPredicate predicate){
+        return Arrays.stream(list).allMatch(predicate);
+    }
+}
